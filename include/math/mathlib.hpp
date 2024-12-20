f960a263ad7fe86f17c7ccd02d8e7fe5cfdd93e2
Neil Chen <neilchen1998@gmail.com>
2024-12-20 16:21:45 -0600

    created generate_permutation func

--- include/math/mathlib.hpp
+++ include/math/mathlib.hpp
@@ -1,6 +1,11 @@
 #ifndef INCLUDE_MATH_MATHLIB_H_
 #define INCLUDE_MATH_MATHLIB_H_
 
+#include <vector>   // std::vector
+#include <string>
+
 double distance(double x1, double y1, double x2, double y2);
+
+std::vector<std::string> generate_permutation(int N);
 
 #endif // INCLUDE_MATH_MATHLIB_H_
--- src/mathlib.cc
+++ src/mathlib.cc
@@ -1,4 +1,9 @@
-#include <cmath>
+#include <cmath>        // square
+#include <algorithm>    // std::prev_permutation, std::
+#include <iostream>     // std::cout
+#include <string>
+
+#include <boost/math/special_functions/factorials.hpp>  //boost::math::factorial
 
 #include "math/mathlib.hpp"
 
@@ -8,4 +13,34 @@ double distance(double x1, double y1, double x2, double y2)
     double y_diff = (y1 - y2);
 
     return sqrt(x_diff * x_diff + y_diff * y_diff);
+}
+
+std::vector<std::string> generate_permutation(int N)
+{
+    std::string s(N, 'a');
+    std::vector<std::string> ret;
+
+    // calculates the size of the permutation vector (the factorial of N)
+    auto sz = boost::math::factorial<double>(N);
+    ret.resize(sz);
+
+    // constructs the base permutation string
+    for (int i = 0; i < N; ++i)
+    {
+        s[i] += i;
+    }
+
+    #if DEBUG
+    std::cout << s << std::endl;
+    #endif
+
+    // finds all the permutations
+    int i = 0;
+    do
+    {
+        ret[i] = s;
+        ++i;
+    } while (std::next_permutation(s.begin(), s.end()));
+    
+    return ret;
 }
